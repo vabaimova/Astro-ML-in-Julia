a0567127a6d278812c15575ee665cf3b13e96b17
vabaimova <stormseecker@gmail.com>
2014-11-26 18:08:12 -0500

    Added fitsHeaderData.jl

---
+++ fitsHeaderData.jl
@@ -0,0 +1,38 @@
+#=
+    fitsHeaderData
+    Copyright © 2014 Vera Abaimova <stormseecker@gmail.com>
+
+    Distributed under terms of the MIT license.
+=#
+
+## Reads in FITS file and takes the necessary Kepler data
+## Formats the data in an array which will eventually be appended
+## to one big file where each row is a star
+
+using FITSIO
+
+function getHeaderData(fileName::String, keywordList::String)
+    
+    fitsFile = FITS(fileName)
+    header = readheader(fitsFile[1])
+    keywordFile = open(keywordList,"r")
+
+    ## Create an array of keywords that contain the desired data
+    keywords = readdlm(keywordFile,String)
+    keywords = map((x) -> normalize_string(x),keywords)
+
+    ## Create array that will contain the extracted data
+    starData = Float64[]
+
+    for keyword in keywords
+        ## Access the value stored in the header at the keyword
+        data = header[keyword] 
+        ## Append the acquired value to the data array
+        append!(starData,[data])
+    end
+
+#    Used for testing the accuracy of code
+#    println("Star Data: ", starData)
+
+end
+
