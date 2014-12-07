e5b30c639e0d8c55da854fd738a7e738d52047ca
vabaimova <stormseecker@gmail.com>
2014-12-07 05:24:42 -0500

    Created a file with functions to test drivers

---
+++ testDrivers.jl
@@ -0,0 +1,22 @@
+#=
+    testDrivers
+    Copyright © 2014 Vera Abaimova <stormseecker@gmail.com>
+
+    Distributed under terms of the MIT license.
+=#
+
+## A program to test the drivers that preprocess the data
+
+include("lightcurveFuncs.jl")
+include("headerData.jl")
+include("preProcessingMain.jl")
+
+function testLCDriver()
+    settingsFile = "CREATIVE_STATION_SETTINGS.txt"
+    mySettings = initializeSettings(settingsFile,1)
+
+    allKIDs = dir_KIDs(settings.fits_dir)
+    firstKID = allKIDs[1]
+
+    lightcurveDriver(mySettings,firstKID,allKIDs)
+end
