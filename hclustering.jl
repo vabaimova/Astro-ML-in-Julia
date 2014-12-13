a4df485e055ab9f3d6ab9640514bfd8e3f0f594a
vabaimova <stormseecker@gmail.com>
2014-12-13 03:30:49 -0500

    Proper clustering and dendro process

--- hclustering.jl
+++ hclustering.jl
@@ -6,6 +6,7 @@
 =#
 
 using PyCall
+using PyPlot
 @pyimport scipy.spatial.distance as dist
 @pyimport scipy.cluster.hierarchy as hier
 
@@ -18,9 +19,19 @@ end
 
 function testing()
     kids,feats = getNormKidsFeats()
-    dist_matrix = dist.pdist(feats,"euclidean")
+    n = integer(sqrt(length(kids)))
+
+    sample = feats[randperm(n),:]
+
+    dist_matrix = dist.pdist(sample,"euclidean")
     link_matrix = hier.linkage(dist_matrix)
-    hier.dendogram(link_matrix)
+
+    color_thres = 0.9*maximum(link_matrix[:,2])
+    hier.dendrogram(link_matrix,show_leaf_counts=true,p=50,
+        truncate_mode="level",distance_sort="ascending",leaf_font_size=13)
+    xlabel("Number of Members",fontsize=15)
+    ylabel("Normalized Distance Between Children Nodes",fontsize=15)
+    title("Dendrogram of Hierarchical Clustering Results",fontsize=17)
 end
 
 testing()
