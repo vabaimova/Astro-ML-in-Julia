11c320fe340569afe3c9cf8e464365ac8cd0f544
vabaimova <stormseecker@gmail.com>
2014-12-14 22:32:14 -0500

    Created bar plot

--- hclustering.jl
+++ hclustering.jl
@@ -21,15 +21,42 @@ function testing()
     println("begun process")
     kids,feats = getNormKidsFeats()
 
-    dist_matrix = dist.pdist(feats,"euclidean")
+    nkids = size(kids)[1]
+    n =  int(sqrt(nkids))
+
+    inds = randperm(nkids)[1:n]
+    sample = feats[inds,:]
+
+    dist_matrix = dist.pdist(sample,"euclidean")
     link_matrix = hier.linkage(dist_matrix)
 
     color_thres = 0.9*maximum(link_matrix[:,2])
-    hier.dendrogram(link_matrix,show_leaf_counts=true,p=50,
+    R = hier.dendrogram(link_matrix,show_leaf_counts=true,p=50,
         truncate_mode="level",distance_sort="ascending",leaf_font_size=13)
     xlabel("Number of Members",fontsize=15)
     ylabel("Normalized Distance Between Children Nodes",fontsize=15)
     title("Dendrogram of Hierarchical Clustering Results",fontsize=17)
+
+    a=figure()
+    leaf_counts = R["ivl"]
+    ## deal with items with ()
+    pinds = find(map((x) -> contains(x,"("),leaf_counts))
+    for index in pinds
+        leaf_counts[index] = leaf_counts[index][2:end-1]
+    end
+
+    lN = size(leaf_counts)[1]
+    labels = int(linspace(1,lN,lN))
+
+    leaf_counts = int(leaf_counts)
+
+    bar(labels,leaf_counts)
+    xlabel("Cluster",fontsize=15)
+    ylabel("Number of Members",fontsize=15)
+    title("Membership of Hierarchical Clustering",fontsize=17)
+
+    
+
 end
 
 testing()
